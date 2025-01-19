aed1b9a4d163af9cb4272fa14cc3add951359151
arase <23aw0103@jec.ac.jp>
2025-01-19 21:49:28 +0900

    タイム変数を親コンポーネントに移行して管理

--- src/app/colorPalette/page.jsx
+++ src/app/colorPalette/page.jsx
@@ -3,12 +3,16 @@
 import React, { useRef, useState, useEffect } from 'react';
 import Canvas from '../cmp/canvas'
 import Tutorial from '../cmp/tutorial'
+import Question from '../cmp/question';
 
 const Page = () => {
 
+  const [timer, setTimer] = useState(true);
+
   return (
-    <div className='relative bg-black w-screen h-screen'>
-      <Tutorial/>
+    <div className='relative w-screen h-screen'>
+      <Tutorial timer={timer} setTimer={setTimer}/>
+      <Question timer={timer}/>
       <Canvas/>
     </div>
   );
