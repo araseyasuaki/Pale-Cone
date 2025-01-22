47168e1c51a3d4b97a1170d2dc44a1a864f0acb4
arase <23aw0103@jec.ac.jp>
2025-01-22 18:15:48 +0900

    supabase.js追加

--- src/app/libs/supabase.js
+++ src/app/libs/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 // サービスロールキーを使用してSupabaseクライアントを作成
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;  // SupabaseのURL
-const supabaseServiceRoleKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;  // サービスロールキー
+const supabaseUrl = process.env.NEXT_PUBLIC_YELL_SUPABASE_URL;  // SupabaseのURL
+const supabaseServiceRoleKey = process.env.NEXT_PUBLIC_YELL_SUPABASE_ANON_KEY;  // サービスロールキー
 
 // サーバー側での設定
 const supabase = createClient(supabaseUrl, supabaseServiceRoleKey);
