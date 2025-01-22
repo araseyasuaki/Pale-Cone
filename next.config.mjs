c443ee45581d1ec74071d6310d945f5e877d2674
arase <23aw0103@jec.ac.jp>
2025-01-22 17:08:59 +0900

    画像追加

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["images.microcms-assets.io"], // 使用する画像ホスト名を追加
+  },
+};
 
 export default nextConfig;
+
+
+
