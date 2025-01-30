73fe713d7db2ff946b6ab2e958314c65379b4a51
arase <23aw0103@jec.ac.jp>
2025-01-31 00:50:21 +0900

    CMSを使い回しができるように統一

--- src/app/libs/microcms.js
+++ src/app/libs/microcms.js
@@ -1,6 +1,37 @@
+import { useState, useEffect } from 'react';
 import { createClient } from 'microcms-js-sdk';
 
 export const client = createClient({
   serviceDomain: process.env.NEXT_PUBLIC_MICROCMS_SERVICE_DOMAIN,
   apiKey: process.env.NEXT_PUBLIC_MICROCMS_API_KEY,
 });
+
+const CmsData = () => {
+  const [userData, setUserData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const limit = 100;
+      let allData = [];
+      let offset = 0;
+      while (true) {
+        const data = await client.get({
+          endpoint: 'works',
+          queries: {
+            limit,
+            offset,
+          },
+        });
+        allData = [...allData, ...data.contents];
+        if (data.contents.length < limit) break;
+        offset += limit;
+      }
+      setUserData(allData);
+    };
+    fetchData();
+  }, []);
+
+  return { userData };
+};
+
+export default CmsData;
